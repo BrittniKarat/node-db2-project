3538ff02e50cda9d6701e29328ce6c385330becb
BunniMK <86545296+BunniMK@users.noreply.github.com>
2022-01-18 16:39:00 -0700

    Getting started

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,3 @@
-// DO YOUR MAGIC
+const router = require('express').Router
+const Cars = require('./cars-model')
+
--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  await knex.schema.createTable('cars', table => {
+    table.increments('car_id')
+    table.text('vin', 18).unique().NotNullable()
+    table.text('make', 30).NotNullable()
+    table.text('model', 30).NotNullable()
+    table.decimal('milage').NotNullable()
+    table.text('title', 20)
+    table.text('transmission', 30)
+  })
 };
 
-exports.down = function (knex) {
-  // DO YOUR MAGIC
+exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists('cars')
 };
